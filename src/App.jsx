71b90c692ed9fc9092ff965fe22a6a0a88620c0c
Lester <jlhebres15@gmail.com>
2026-02-13 08:02:10 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Expense from "./pages/Dashboard/Expense";
+import Income from "./pages/Dashboard/Income";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+         <Route path="/" element={<Root />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/income" element={<Income />} />
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+
+
+const Root = () => {
+  //Check if token exist in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+  
+  //Redirect todashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
